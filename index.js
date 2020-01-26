ab6ca4dd86cdaf577b79553d360c343ed48a7bf6
pareeksp47 <45043001+pareeksp47@users.noreply.github.com>
2020-01-26 19:04:10 +0100

    Fixes with test case

--- index.js
+++ index.js
@@ -1,6 +1,88 @@
 import 'regenerator-runtime/runtime'
 
-import app from './app'
+import { getClinic } from './apiClinic'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (_req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+app.get('/api/getStudents', async (req, res) => {
+
+    const clinic = getClinic();
+    const createdElements = await clinic.getAllStudents()
+    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
+})
+
+
+app.get('/api/getHouses', async (req, res) => {
+
+    const createdElements = await getClinic().getAllHouses()
+    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
+})
+
+app.get('/api/getPoints', async (req, res) => {
+
+    const createdElements = await getClinic().getAllPoints()
+    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
+})
+
+
+app.post('/api/points/:nbpoints/:idprofessor/:idhouse', async (req, res) => {
+
+    const nbpoints = req.params.nbpoints;
+    const idprofessor = req.params.idprofessor;
+    const idhouse = req.params.idhouse;
+
+    let statusCode = 400;
+
+    if (nbpoints && idprofessor && idhouse) {
+
+        statusCode = await getClinic().addRemovePoints(nbpoints, idprofessor, idhouse);
+
+    }
+    res.status(statusCode).send();
+})
+
+app.post('/api/createStudent/:firstname/:lastname/:gender/:id_house', async (req, res) => {
+
+    const firstname = req.params.firstname
+    const lastname = req.params.lastname
+    const gender = req.params.gender
+    const id_house = parseInt(req.params.id_house)
+
+    const createdElements = await getClinic().createStudent(firstname, lastname, gender, id_house)
+    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
+})
+
+app.post('/api/createProfessor/:firstname/:lastname/:gender', async (req, res) => {
+
+    const firstname = req.params.firstname
+    const lastname = req.params.lastname
+    const gender = req.params.gender
+
+    const createdElements = await getClinic().createProfessor(firstname, lastname, gender)
+    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
+})
+
+
+app.get('/api/getProfessors', async (req, res) => {
+
+    const createdElements = await getClinic().getAllProfessors()
+    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
+})
+
+
 
 const port = process.env.PORT || 5000;
 
@@ -8,3 +90,7 @@ const server = app.listen(port, () => {
   const port = server.address().port
   console.log('Server listening on port ' + port + '...')
 })
+
+
+
+export default app
