1119bb1e486440097cfb45e4fddea029503c11f8
ramsha.sajid <ramshasajid93@gmail.com>
2020-01-26 17:17:54 +0100

    Add unit test for api

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-import {getClinic} from './apiClinic'
+import { getClinic } from './apiClinic'
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -18,7 +18,8 @@ app.use(function (_req, res, next) {
 
 app.get('/api/getStudents', async (req, res) => {
 
-    const createdElements = await getClinic().getAllStudents()
+    const clinic = getClinic();
+    const createdElements = await clinic.getAllStudents()
     res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
 })
 
@@ -39,36 +40,36 @@ app.get('/api/getPoints', async (req, res) => {
 app.post('/api/points/:nbpoints/:idprofessor/:idhouse', async (req, res) => {
 
     const nbpoints = req.params.nbpoints;
-	const idprofessor = req.params.idprofessor;
-	const idhouse = req.params.idhouse;
-	
-	let statusCode = 400 ;
-	
-	if(nbpoints && idprofessor && idhouse){
-		
-		statusCode = await getClinic().addRemovePoints(nbpoints,idprofessor,idhouse);
-		
-	}
-	res.status(statusCode).send();
+    const idprofessor = req.params.idprofessor;
+    const idhouse = req.params.idhouse;
+
+    let statusCode = 400;
+
+    if (nbpoints && idprofessor && idhouse) {
+
+        statusCode = await getClinic().addRemovePoints(nbpoints, idprofessor, idhouse);
+
+    }
+    res.status(statusCode).send();
 })
 
 app.post('/api/createStudent/:firstname/:lastname/:gender/:id_house', async (req, res) => {
-  
+
     const firstname = req.params.firstname
     const lastname = req.params.lastname
     const gender = req.params.gender
     const id_house = parseInt(req.params.id_house)
-    
-    const createdElements = await getClinic().createStudent(firstname, lastname, gender,id_house)
+
+    const createdElements = await getClinic().createStudent(firstname, lastname, gender, id_house)
     res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
 })
 
 app.post('/api/createProfessor/:firstname/:lastname/:gender', async (req, res) => {
-  
+
     const firstname = req.params.firstname
     const lastname = req.params.lastname
     const gender = req.params.gender
-    
+
     const createdElements = await getClinic().createProfessor(firstname, lastname, gender)
     res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
 })
@@ -83,3 +84,5 @@ app.get('/api/getProfessors', async (req, res) => {
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+export default app
